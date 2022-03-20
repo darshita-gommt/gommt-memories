cf078c8e3ae7f8afbac6a3a4cb766313da2bd250
darshita.golchha <darshita.golchha@go-mmt.com>
2022-03-20 15:23:44 +0530

    Added github api to store and fetch data

---
+++ src/apiCient/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const axiosInstance = axios.create();
+
+axiosInstance.defaults.headers.common['content-type'] = 'application/x-www-form-urlencoded';
+  
+const baseURL = 'https://varungommt.github.io/AppData/';
+
+export const getData = () => {
+    axiosInstance.get(baseURL + 'userData.json').then(response => {
+        console.log('Response', response);
+    }).catch(console.error)
+}
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,10 +2,13 @@ import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 import SplashScreen from "react-native-splash-screen";
 
+import { getData } from "../apiCient";
+
 const HomeScreen = () => {
     useEffect(() => {
+      // to fetch user data
+      getData();
         SplashScreen.hide();
-
     }, []);
 
     return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
