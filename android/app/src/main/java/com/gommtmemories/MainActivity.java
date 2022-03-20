d62b6edf5c6f8770bc6147dcc1052bdb6667f9e3
darshita.golchha <darshita.golchha@go-mmt.com>
2022-03-20 14:40:00 +0530

    Added splash screen

--- android/app/src/main/java/com/gommtmemories/MainActivity.java
+++ android/app/src/main/java/com/gommtmemories/MainActivity.java
@@ -1,6 +1,8 @@
 package com.gommtmemories;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +13,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "gommtMemories";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this); 
+    super.onCreate(null);
   }
 }
